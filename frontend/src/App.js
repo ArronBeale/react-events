58898e6b760d704ff9f94f15b5d917b794f2c3fe
Arron <beale86@msn.com>
2023-08-29 18:25:21 +0000

    Reconfigure router paths in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,14 @@
 //    - EventDetailPage
 //    - NewEventPage
 //    - EditEventPage
+// DONE
 // 2. Add routing & route definitions for these five pages
 //    - / => HomePage
 //    - /events => EventsPage
 //    - /events/<some-id> => EventDetailPage
 //    - /events/new => NewEventPage
 //    - /events/<some-id>/edit => EditEventPage
+// DONE
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
@@ -30,21 +32,15 @@ import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <HomePage /> }, // path: ''
-      { path: 'events', element: <EventsPage /> },
-      { path: 'new', element: <NewEventPage /> },
-      { path: 'events/:event.id/edit', element: <EditEventPage /> },
-      { path: 'events/:eventId', element: <EventDetailPage /> },
-    ],
-  },
+  { path: '/', element: <HomePage /> },
+  { path: '/events', element: <EventsPage /> },
+  { path: '/events/:eventId', element: <EventDetailPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+  { path: '/events/:eventId/edit', element: <EditEventPage /> },
 ]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

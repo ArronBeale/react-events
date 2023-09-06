2a04318e87dec7d395507840d641a6f6e51e147b
Arron <beale86@msn.com>
2023-09-06 19:07:33 +0000

    Add loader to EventDetail.js, import useLoaderData to view event details

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,14 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
 
     return (
-      <>
-        <h1>Event Details</h1>
-        <p>Event ID: {params.eventId}</p>
-      </>
-    )
+      <EventItem event={data.event} />
+    );
   }
   
   export default EventDetailPage;
+
+  export async function loader({reqeust, params}) {
+    const id = params.eventId;
+
+    const response = await fetch('https://8080-arronbeale-reactevents-xvwen2ceig2.ws-eu104.gitpod.io/events/' + id);
+    
+    if (!response.ok) {
+      throw json({message: 'Could not fetch details for selected event.'},
+      {
+        status: 500
+      })
+    } else {
+      return response;
+    }
+  }
